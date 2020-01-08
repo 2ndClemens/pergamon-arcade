2d4fef1db1267d275c814bf9689f2c4388d24d36
2ndClemens <45330624+2ndClemens@users.noreply.github.com>
2020-01-08 21:33:07 +0100

    shadows

--- main.ts
+++ main.ts
@@ -1374,40 +1374,40 @@ c c c c c c c c c c c . . . . .
     //% blockIdentity=images._tile
     export const tile74 = img`
 b b b b b b b b c b b b b b b b 
-d d d d d d d c d d d d d d d d 
-b b b b b b c d d d d d d d d d 
-c c c c c c c d d d d d d d d d 
+d d d d d d d c b b b d d d d d 
+b b b b b b c b b b d d d d d d 
+c c c c c c c b b b d d d d d d 
 d d d d d c b b b b b b b b b b 
-d d d d d c d d d d d d d d d d 
-c c c c c c d d d d d d d d d d 
-c b b c d d d d d d d d d d d d 
+d d d d d c b b b d d d d d d d 
+c c c c c c b b d d d d d d d d 
+c b b c b b b d d d d d d d d d 
 c b b c b b b b b b b b b b b b 
-c d d c d d d d d d d d d d d d 
-b b b c d d d d d d d d d d d d 
-c c c c d d d d d d d d d d d d 
+c d d c b b d d d d d d d d d d 
+b b b c b b d d d d d d d d d d 
+c c c c b b d d d d d d d d d d 
 c b b b b b b b b b b b b b b b 
-b d d d d d d d d d d d d d d d 
-b d d d d d d d d d d d d d d d 
-c d d d d d d d d d d d d d d d 
+b b b d d d d d d d d d d d d d 
+b b b d d d d d d d d d d d d d 
+c b b d d d d d d d d d d d d d 
 `
     //% blockIdentity=images._tile
     export const tile76 = img`
 b b b b b b b c b b b b b b b b 
-d d d d d d d d c d d d d d d d 
-d d d d d d d d d c b b b b b b 
-d d d d d d d d d c c c c c c c 
+d d d d d b b b c d d d d d d d 
+d d d d d d b b b c b b b b b b 
+d d d d d d b b b c c c c c c c 
 b b b b b b b b b b c d d d d d 
-d d d d d d d d d d c d d d d d 
-d d d d d d d d d d c c c c c c 
-d d d d d d d d d d d d c b b c 
+d d d d d d d b b b c d d d d d 
+d d d d d d d b b b c c c c c c 
+d d d d d d d d d b b b c b b c 
 b b b b b b b b b b b b c b b c 
-d d d d d d d d d d d d c d d c 
-d d d d d d d d d d d d c b b b 
-d d d d d d d d d d d d c c c c 
+d d d d d d d d d b b b c d d c 
+d d d d d d d d d b b b c b b b 
+d d d d d d d d d b b b c c c c 
 b b b b b b b b b b b b b b b c 
-d d d d d d d d d d d d d d d b 
-d d d d d d d d d d d d d d d b 
-d d d d d d d d d d d d d d d c 
+d d d d d d d d d d d d d b b b 
+d d d d d d d d d d d d d b b b 
+d d d d d d d d d d d d b b b c 
 `
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Event, function (sprite, otherSprite) {
