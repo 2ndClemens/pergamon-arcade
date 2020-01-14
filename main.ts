bd5c13e634e6114660d8504b7399fb37706ac11c
2ndClemens <45330624+2ndClemens@users.noreply.github.com>
2020-01-15 00:05:59 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2282,7 +2282,8 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
             nike.x = athena.x - 12
             nike.y = athena.y - 30
             game.showLongText("Great. Athena's statue is complete again.", DialogLayout.Bottom)
-            pause(1000)
+            effects.confetti.startScreenEffect()
+            pause(2000)
             game.over(true, effects.hearts)
         }
     }
